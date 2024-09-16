b5e9bca99ec537808868d1b6a13396cf04a3a9ce
Lyzd1 <Luoyezhidao123@protonmail.com>
2024-09-16 20:00:46 +0800

    auto selcect based on danmaku

--- api.lua
+++ api.lua
@@ -3,22 +3,29 @@
 -- 导入ass数据相关流程
 local options = {
     load_more_danmaku = false,
+	auto_load = false,
 }
+
 
 require("mp.options").read_options(options, "uosc_danmaku")
 
 local utils = require("mp.utils")
 local episodeId = nil
 
+
 -- Buffer for the input string
 local input_buffer = ""
 local danmaku_path = mp.get_script_directory() .. "/danmaku/"
 
 
+local tmp=options.auto_load
+local tmp_flag=false  --标记自动装载弹幕流程是否被跑通。如果跑通，则置位为true。如果未跑通,则为false。
+                      --在打开视频时flag=false，此时在使用快捷键d时会识别到tmp_flag=false,会自动调用命令执行一次autoload装载操作。
 
 
 
--- url编码转换  
+
+-- url编码转换
 function url_encode(str)
     -- 将非安全字符转换为百分号编码
     if str then
@@ -182,67 +189,96 @@ function write_json_file(file_path, data)
 end
 
 
-
 -- 获取父文件名
 function get_father_directory()
-	local file_path = mp.get_property("path")  --获取当前视频文件的完整路径
-    local fname = string.match(file_path, ".*\\([^\\]+)\\[^\\]+$")  -- 获取最后一个文件夹名称
+    local file_path = mp.get_property("path") --获取当前视频文件的完整路径
+    local cwd = mp.get_property("working-directory")
+    local fname = nil
+    local full_path
+    if platform == "windows" then
+        file_path = string.gsub(file_path, "^%.\\", "")
+        if string.find(file_path, "\\") == nil then
+            full_path = cwd .. "\\" .. file_path
+        else
+            full_path = file_path
+        end
+        fname = string.match(full_path, ".*\\([^\\]+)\\[^\\]+$")
+    else
+        file_path = string.gsub(file_path, "^%./", "")
+        if string.find(file_path, "/") == nil then
+            full_path = cwd .. "/" .. file_path
+        else
+            full_path = file_path
+        end
+        fname = string.match(full_path, ".*/([^/]+)/[^/]+$")
+    end
     return fname
 end
 
 
 -- 获取当前文件名所包含的集数
 function get_episode_number()
-	local filename = mp.get_property("filename")
-	local pattern = "(%d+)"
-	
-	 -- 尝试匹配文件名中的数字
+    local filename = mp.get_property("filename")
+    local pattern = "(%d+)"
+
+    -- 尝试匹配文件名中的数字
     for number in string.gmatch(filename, pattern) do
         -- 转换为数字
         local episodeNumber = tonumber(number)
-        -- 检查数字是否大于2000，以及是否是4k或1080p
-        if episodeNumber and episodeNumber <= 2000 and filename:sub(filename:find(number) + #number + 1, filename:find(number) + #number + 1) ~= "k" and filename:sub(filename:find(number) + #number + 1, filename:find(number) + #number + 4) ~= "1080p" then
-			return episodeNumber
+        -- 最大集数1000，过滤4k和1080p
+        filename = string.lower(filename)
+        if
+            episodeNumber
+            and episodeNumber <= 1000
+            and filename:sub(filename:find(number) + 0, filename:find(number) + #number + 1) ~= "4k"
+            and filename:sub(filename:find(number) + 0, filename:find(number) + #number + 1) ~= "720p"
+            and filename:sub(filename:find(number) + 0, filename:find(number) + #number + 1) ~= "360p"
+        then
+            return episodeNumber
         end
     end
-	
 end
 
 
 
-mp.register_event("start-file", get_episode_number)
-
--- 写入history.json
--- 读取episodeId获取danmaku
-function set_episode_id(input)
+--	写入history和读取弹幕库
+--	对不同调用模式进行分流。
+function set_episode_id(input,from_menu)   --设置为菜单建立
+	from_menu = from_menu or false  --如果为true，则使用下面逻辑。如果为false，则不修改数据，仅加载。
     episodeId = tonumber(input)
-	local fname = get_father_directory()
-    --将文件名:episodeId写入history.json
-	if fname ~= nil then
-        local history_path = danmaku_path .. "history.json"
-        local history_json = read_file(history_path)
-        if history_json ~= nil then
-			local history = utils.parse_json(history_json)
-			local episodeNumber=get_episode_number()   --动漫的集数 
-			history[fname] = {}
-			history[fname].episodeNumber=episodeNumber
-			history[fname].episodeId = episodeId
-			write_json_file(history_path, history)
-		end	
-			
-        else
-            local history = {}
-            history[fname] = {}
-			history[fname].episodeNumber=episodeNumber
-			history[fname].episodeId = episodeId
-            write_json_file(history_path, history)
-        
-    end
-    if options.load_more_danmaku then
-        fetch_danmaku_all(episodeId) 
-    else
-        fetch_danmaku(episodeId)
-    end
+	  
+	
+	-- 如果来自菜单加载弹幕，正常写入
+	if from_menu then 
+		local fname = get_father_directory()	--获取父文件名
+		if fname ~= nil then
+			local history_path = danmaku_path .. "history.json"  
+			local episodeNumber=get_episode_number()   
+			local history_json = read_file(history_path)
+			if history_json ~= nil then
+				local history = utils.parse_json(history_json)
+				history[fname] = {}
+				history[fname].episodeNumber=episodeNumber
+				history[fname].episodeId = episodeId
+				write_json_file(history_path, history)
+			else
+				local history = {}
+				history[fname] = {}
+				history[fname].episodeNumber=episodeNumber
+				history[fname].episodeId = episodeId
+				write_json_file(history_path, history)
+			end
+		end
+	end	
+	
+
+	-- 读取episodeID去获取弹幕
+	if options.load_more_danmaku then
+			fetch_danmaku_all(episodeId) 
+		else
+			fetch_danmaku(episodeId)
+		end
+
 end
 
 
@@ -324,7 +360,7 @@ function fetch_danmaku_all(episodeId)
         cancellable = false,
     }
 
-    mp.osd_message("弹幕加载中...", 60)
+    mp.osd_message("弹幕加载中...", 3)
 
     local res = utils.subprocess(req)
 
@@ -360,7 +396,7 @@ function fetch_danmaku_all(episodeId)
 
 
         --mp.osd_message("正在从此地址加载弹幕：" .. related["url"], 60)  
-		mp.osd_message("正在从第三方库装填弹幕", 60)  
+		mp.osd_message("正在从第三方库装填弹幕", 2)  
 
         res = utils.subprocess(req)
 
@@ -392,47 +428,50 @@ function fetch_danmaku_all(episodeId)
         ::continue::
     end
 
-    url = "https://api.dandanplay.net/api/v2/comment/" .. episodeId .. "?withRelated=false&chConvert=0"
+	-- 	如果成功加载第三方弹幕库，就不适用danmaku
+	if comments == nil then 
+		url = "https://api.dandanplay.net/api/v2/comment/" .. episodeId .. "?withRelated=false&chConvert=0"
 
-    req = {
-        args = {
-            "curl",
-            "-L",
-            "-X",
-            "GET",
-            "--header",
-            "Accept: application/json",
-            "--header",
-            "User-Agent: MyCustomUserAgent/1.0",
-            url,
-        },
-        cancellable = false,
-    }
+		req = {
+			args = {
+				"curl",
+				"-L",
+				"-X",
+				"GET",
+				"--header",
+				"Accept: application/json",
+				"--header",
+				"User-Agent: MyCustomUserAgent/1.0",
+				url,
+			},
+			cancellable = false,
+		}
 
-    mp.osd_message("正在从弹弹Play库装填弹幕", 60)
+		mp.osd_message("正在从弹弹Play库装填弹幕", 1)
 
-    res = utils.subprocess(req)
+		res = utils.subprocess(req)
 
-    if res.status ~= 0 then
-        mp.osd_message("HTTP Request failed: " .. res.error, 3)
-        return
-    end
+		if res.status ~= 0 then
+			mp.osd_message("HTTP Request failed: " .. res.error, 3)
+			return
+		end
 
-    response = utils.parse_json(res.stdout)
+		response = utils.parse_json(res.stdout)
 
-    if not response or not response["comments"] then
-        mp.osd_message("No result", 3)
-        return
-    end
+		if not response or not response["comments"] then
+			mp.osd_message("No result", 3)
+			return
+		end
 
-    for _, comment in ipairs(response["comments"]) do
-        table.insert(comments, comment)
-    end
+		for _, comment in ipairs(response["comments"]) do
+			table.insert(comments, comment)
+		end
 
-    if #comments == 0 then
-        mp.osd_message("该集弹幕内容为空，结束加载", 3)
-        return
-    end
+		if #comments == 0 then
+			mp.osd_message("该集弹幕内容为空，结束加载", 3)
+			return
+		end
+	end		
 
     local success = save_json_for_factory(comments)
     if success then
@@ -447,6 +486,7 @@ function fetch_danmaku_all(episodeId)
         mp.osd_message("Error saving JSON file", 3)
     end
 end
+
 
 
 --通过输入源url获取弹幕库 
@@ -616,7 +656,7 @@ function convert_with_danmaku_factory()
         danmaku_path .. "danmaku.ass",
         "-i",
         danmaku_path .. "danmaku.json",
-		--[[ 字体大小
+		--字体大小
 		"--fontsize",
 		"40",
 		-- 字体描边深度 0-4
@@ -634,10 +674,10 @@ function convert_with_danmaku_factory()
 		-- 弹幕重叠  
 		"--density",
 		"-1",
-		-- 滚动弹幕显示范围  0.0-1.0 
-		"--scrollarea",
+		-- 全部弹幕的显示范围  0.0-1.0   |  滚动弹幕：--scrollarea 
+		"--displayarea",
 		"0.2",
-		"--ignore-warnings",  ]]--
+		"--ignore-warnings",  
     }
 
     utils.subprocess({ args = cmd })
@@ -672,32 +712,109 @@ os.remove(rm1)
 os.remove(rm2)
 
 
+
+
+
 -- 自动加载上次匹配的弹幕
 function auto_load_danmaku()
-    local fname = get_father_directory()
-    if fname ~= nil then
-        local history_path = danmaku_path .. "history.json"
-        local history_json = read_file(history_path)
-        if history_json ~= nil then
-            local history = utils.parse_json(history_json)
-			-- 1.判断父文件名是否存在
-			local history_fname = history[fname]
-			print(history_fname) 
-			if history_fname ~= nil then
-				--2.如果存在，则获取number和id
-				local history_number=history[fname].episodeNumber
-				local history_id = history[fname].episodeId  
-				local playing_number = get_episode_number()
-				local x=playing_number - history_number  --获取集数差值
-				local tmp_id = tostring(x+history_id)	
-				mp.osd_message("自动加载上次匹配的弹幕", 60)
-				set_episode_id(tmp_id)
+	tmp_flag=false
+	if tmp then 
+		local fname = get_father_directory()
+		if fname ~= nil then
+			local history_path = danmaku_path .. "history.json"
+			local history_json = read_file(history_path)
+			if history_json ~= nil then
+				local history = utils.parse_json(history_json)
+				-- 1.判断父文件名是否存在
+				local history_fname = history[fname]
+				if history_fname ~= nil then
+					--2.如果存在，则获取number和id
+					local history_number=history[fname].episodeNumber
+					local history_id = history[fname].episodeId  
+					local playing_number = get_episode_number()
+					local x=playing_number - history_number  --获取集数差值
+					local tmp_id = tostring(x+history_id)			
+					mp.osd_message("自动加载上次匹配的弹幕", 60)
+					set_episode_id(tmp_id)
+				end
 			end
-        end
-    end
+			tmp_flag=true  --执行获取弹幕操作后，置位为true
+		end
+	end
 end
 
+
+function write_flag()
+    --打开文件
+    local file = io.open("./portable_config/script-opts/uosc_danmaku.conf", "r")
+    if not file then
+        print("无法打开文件")
+        return
+    end	
+
+    -- 读取文件内容
+    local content = file:read("*all")
+    file:close()
+
+    -- 查找flag字段
+    local flag_line = string.match(content, "auto_load=(%w+)")
+    if not flag_line then
+        mp.osd_message("未找到auto_load字段", 3)
+        return
+    end
+
+    -- 切换flag值
+    local new_flag = flag_line == "yes" and "no" or "yes"
+
+    -- 替换文件中的flag值
+    local new_content = string.gsub(content, "auto_load=(%w+)", "auto_load=" .. new_flag)
+
+    -- 保存修改后的文件内容
+    local new_file = io.open("./portable_config/script-opts/uosc_danmaku.conf", "w")
+    if not new_file then
+        mp.osd_message("auto_load无法写入", 3)
+        return
+    end
+    new_file:write(new_content)
+    new_file:close()
+
+end
+
+
+
+
+-- 定义一个函数来切换启动和取消启动状态
+-- 1.写入记录flag状态。(修改本地文件的auto_load值)
+-- 2.翻转tmp，并且识别tmp_flag
+-- 3.做对应操作
+function toggle_auto_load()
+	write_flag()
+	tmp = not tmp
+	if tmp then
+		if tmp_flag then 
+		mp.osd_message("开启自动装载", 2)
+		show_danmaku_func()
+        mp.commandv("script-message-to", "uosc", "set", "show_danmaku", "on")
+		else 
+		mp.osd_message("开启自动装载", 2)
+		mp.commandv("script-message", "auto_load_danmaku")
+		show_danmaku_func()
+        mp.commandv("script-message-to", "uosc", "set", "show_danmaku", "on")
+		end
+	else 
+		mp.osd_message("关闭自动装载", 2)
+		hide_danmaku_func()
+        mp.commandv("script-message-to", "uosc", "set", "show_danmaku", "off")	
+	end 
+end
+
+
+-- 打开新文件时，总是执行。
 mp.register_event("start-file", auto_load_danmaku)
+-- 绑定函数
+mp.register_script_message("auto_load_danmaku", auto_load_danmaku)
+-- 绑定快捷键
+mp.add_forced_key_binding("d", "toggle_auto_load", toggle_auto_load)
 
 
 
--- main.lua
+++ main.lua
@@ -1,7 +1,9 @@
+-- 引入模块
 local utils = require("mp.utils")
 require("api")
 
-function get_animes(query)
+
+function get_animes(query) --query是动漫的部分名称
     local encoded_query = url_encode(query)
 
     local url = "https://api.dandanplay.net/api/v2/search/episodes"
@@ -31,7 +33,7 @@ function get_animes(query)
         mp.osd_message("HTTP Request failed: " .. res.error, 3)
     end
 
-    local response = utils.parse_json(res.stdout)
+    local response = utils.parse_json(res.stdout) --stdout标准输出流
 
     if not response or not response.animes then
         mp.osd_message("无结果", 3)
@@ -40,19 +42,22 @@ function get_animes(query)
 
     mp.osd_message("", 0)
 
+-- 将动漫列表获取到的动漫前缀+episodeId+episodeTitle封存在items。
     local items = {}
     for _, anime in ipairs(response.animes) do
         table.insert(items, {
             title = anime.animeTitle,
+			-- value所包含的信息只有在被选中时才会执行。
             value = {
                 "script-message-to",
                 mp.get_script_name(),
                 "search-episodes-event",
-                utils.format_json(anime.episodes),
+                utils.format_json(anime.episodes),    --episodes包含了episodeId和episodeTitle
             },
         })
     end
 
+-- 依据items参数创建一个二级菜单，submit绑定search-anime-event事件
     local menu_props = {
         type = "menu_anime",
         title = "在此处输入动画名称",
@@ -67,7 +72,10 @@ function get_animes(query)
     mp.commandv("script-message-to", "uosc", "open-menu", json_props)
 end
 
+
+-- 什么时候使用get_episodes
 function get_episodes(episodes)
+-- 将一部动漫的title名字所对应的所有集数组成表items
     local items = {}
     for _, episode in ipairs(episodes) do
         table.insert(items, {
@@ -78,6 +86,7 @@ function get_episodes(episodes)
         })
     end
 
+--通过items创建集数的三级菜单
     local menu_props = {
         type = "menu_episodes",
         title = "剧集信息",
@@ -88,7 +97,7 @@ function get_episodes(episodes)
     mp.commandv("script-message-to", "uosc", "open-menu", json_props)
 end
 
--- 打开输入菜单
+-- 打开输入菜单  ，依据items创建一个一级菜单
 function open_input_menu()
     local menu_props = {
         type = "menu_danmaku",
@@ -96,7 +105,7 @@ function open_input_menu()
         search_style = "palette",
         search_debounce = "submit",
         on_search = { "script-message-to", mp.get_script_name(), "search-anime-event" },
-        footnote = "使用enter或ctrl+enter进行搜索",
+        footnote = "使用enter或ctrl+enter进行搜索", --没太大用处
         items = {
             {
                 value = "",
@@ -133,6 +142,9 @@ function open_add_menu()
     mp.commandv("script-message-to", "uosc", "open-menu", json_props)
 end
 
+
+
+-- 添加了一个弹幕搜索按钮  open_search_danmaku_menu作为关键语句
 mp.commandv(
     "script-message-to",
     "uosc",
@@ -145,6 +157,8 @@ mp.commandv(
     })
 )
 
+
+--主要通过一个url手动添加该集的弹幕
 mp.commandv(
     "script-message-to",
     "uosc",
@@ -157,30 +171,47 @@ mp.commandv(
     })
 )
 
+
+
+
+-- 1.注册一个专有名词，并绑定函数|匿名函数。 
+-- 2.当该专有名词被调用时，会使用后面的函数。
 -- 注册函数给 uosc 按钮使用
-mp.register_script_message("open_search_danmaku_menu", open_input_menu)
-mp.register_script_message("search-anime-event", function(query)
-    mp.commandv("script-message-to", "uosc", "close-menu", "menu_danmaku")
-    get_animes(query)
+mp.register_script_message("open_search_danmaku_menu", open_input_menu)  --将搜索按钮与输入菜单绑定
+mp.register_script_message("search-anime-event", function(query)  --query是输入的动漫名部分。
+    mp.commandv("script-message-to", "uosc", "close-menu", "menu_danmaku")  --关闭输入菜单
+    get_animes(query)  --将部分动漫名传参给get
 end)
-mp.register_script_message("search-episodes-event", function(episodes)
+mp.register_script_message("search-episodes-event", function(episodes) --episodes是episodeId和episodeTitle集合
     mp.commandv("script-message-to", "uosc", "close-menu", "menu_anime")
-    get_episodes(utils.parse_json(episodes))
+    get_episodes(utils.parse_json(episodes)) 
 end)
 
 -- Register script message to show the input menu
 mp.register_script_message("load-danmaku", function(episodeId)
-    set_episode_id(episodeId)
+    set_episode_id(episodeId,true)   --使用api.lua中的函数获取episodeid
 end)
+
+
+
 
 mp.register_script_message("open_add_source_menu", open_add_menu)
-mp.register_script_message("add-source-event", function (query)
-    mp.commandv("script-message-to", "uosc", "close-menu", "menu_source")
-    add_danmaku_source(query)
+mp.register_script_message("add-source-event", function (query)  --确实返回了一个输入的url
+    mp.commandv("script-message-to", "uosc", "close-menu", "menu_source") 
+    add_danmaku_source(query)   --b站番剧链接获取的信息少了关键字，不能通过这里获取弹幕。
 end)
 
-mp.commandv("script-message-to", "uosc", "set", "show_danmaku", "off")
-mp.register_script_message("set", function(prop, value)
+
+
+
+
+
+
+
+-- 显示弹幕 on/off开关  cycle:toggle_on:show_danmaku@uosc_danmaku:on=toggle_on/off=toggle_off?弹幕开关
+-- show_danmaku是调用的api方法。
+mp.commandv("script-message-to", "uosc", "set", "show_danmaku", "off")  --随mpv启动
+mp.register_script_message("set", function(prop, value)  
     if prop ~= "show_danmaku" then
         return
     end
@@ -190,9 +221,9 @@ mp.register_script_message("set", function(prop, value)
     else
         hide_danmaku_func()
     end
-
     mp.commandv("script-message-to", "uosc", "set", "show_danmaku", value)
 end)
+
 mp.register_script_message("show_danmaku_keyboard", function()
     local has_danmaku = false
     local sec_sid = mp.get_property("secondary-sid")
@@ -210,9 +241,32 @@ mp.register_script_message("show_danmaku_keyboard", function()
 
     if sec_sid ~= "no" then
         hide_danmaku_func()
-        mp.commandv("script-message-to", "uosc", "set", "show_danmaku", "off")
+        mp.commandv("script-message-to", "uosc", "set", "show_danmaku", "off")  
     else
         show_danmaku_func()
         mp.commandv("script-message-to", "uosc", "set", "show_danmaku", "on")
     end
+	
+	
 end)
+
+
+
+
+
+
+
+-- 2. 当收到来自bilibili的视频流时，自动加载弹幕 
+function bilibiliauto()
+    local file_path = mp.get_property('path')
+    if string.find(file_path, "bilibili.com") then
+        print(file_path)
+		mp.commandv("script-message", "add-source-event",file_path)
+    end
+end
+
+function delayed_bilibiliauto()
+    mp.add_timeout(1, bilibiliauto) -- Wait for 1 second before trying to get the file_path again
+end
+
+ mp.register_event("start-file", delayed_bilibiliauto)
